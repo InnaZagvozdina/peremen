67ae6b09bedb5f92956e7da449f50279c56c2994
Inna <inna-asbest@mail.ru>
2022-08-01 18:48:05 +0500

    Домашнее задание. Переменные

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
      // Задание 1
+      byte a = -10;
+      short b = 55;
+      int c = 330000;
+      long d = 88l;
+
+      float e = 8.4f;
+      double f = -834.4;
+
+      // Задание 2
+
+        double Боксер1 = 78.2;
+        double Боксер2 = 82.7;
+        double ВесОбщий = Боксер1 + Боксер2;
+        System.out.println("Вес двух боксеров " + ВесОбщий +" кг");
+
+
+
+
+
 
 
     }
